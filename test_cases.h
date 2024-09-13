7d3786213a0e6f3eaa1cf8e77574b14ad6c1001c
uu60 <djz6660@icloud.com>
2024-09-13 00:21:13 -0400

    update case

--- test_cases.h
+++ test_cases.h
@@ -33,8 +33,8 @@ void test_AdditionShareExecutor_0() {
 void test_RsaOtMultiplicationShareExecutor_1() {
     int x, y;
     if (!Mpi::isCalculator()) {
-        x = Math::rand32(0, 100);
-        y = Math::rand32(0, 100);
+        x = Math::rand32();
+        y = Math::rand32();
         Log::i("Multiplier: " + std::to_string(x) + " and " + std::to_string(y));
     }
     RsaOtMultiplicationShareExecutor e(x, y, 32);
@@ -88,9 +88,25 @@ void test_ArrayAddition_4() {
 }
 
 void test_Operators_5() {
-    int a = Math::rand32();
+    int a = Math::rand32(0, 100);
+    if (Mpi::isDataHolder()) {
+        Log::i("Origin: " + to_string(a));
+    }
     int64_t res = IntSecret(a, 32).share().add(15).add(-15).reconstruct().get();
-    Log::i(to_string(res));
+    if (Mpi::isDataHolder()) {
+        Log::i("Reconstruct result: " + to_string(res));
+    }
+
+    int m0 = Math::rand32();
+    int m1 = Math::rand32();
+    if (Mpi::isDataHolder()) {
+        Log::i("m0: " + to_string(m0));
+        Log::i("m1: " + to_string(m1));
+    }
+    int64_t res1 = IntSecret(m0, 32).share().multiply(IntSecret(m1, 32).share().get()).reconstruct().get();
+    if (Mpi::isDataHolder()) {
+        Log::i("Multiplication result: " + to_string(res1));
+    }
 }
 
 #endif //DEMO_TEST_CASES_H
